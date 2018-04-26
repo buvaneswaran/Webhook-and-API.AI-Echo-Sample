4704a5f3f768dea8e05390cde1f1d03fad9a2090
ResulticksInc <mobile.sdk@resulticks.com>
2018-04-26 18:17:18 +0530

    issues

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ restService.post("/echo", function(req, res) {
     ? req.body.result.parameters.echoText
     : "Seems like some problem. Speak again.";
 
-  var speech ="Please repeat"
+  var speech ="Please repeat";
 
   if (factCategory == 'hot day' || factCategory == 'hot today' || factCategory == 'hot' || factCategory == 'today was hot' || factCategory == 'it was a hot day'
   || factCategory == 'it was so hot today' || factCategory == 'it was hot today' || factCategory == 'it was a hot day today'
