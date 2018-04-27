39ec3d885a2dd0d351891ddb58a3dcf7290c4069
ResulticksInc <mobile.sdk@resulticks.com>
2018-04-27 10:28:20 +0530

    issues

--- index.js
+++ index.js
@@ -80,15 +80,7 @@ else if(factCategory == 'thanks' || factCategory == 'ok thanks' || factCategory 
   });
 });
 
-getWeather(query) {
-  const endpoint = 'http://resulticks.biz:81/Home/Register?id=125gh';
-  return this.http
-      .get(endpoint)//, {search: searchParams})
-      .map(res => res.json().main)
-      .subscribe(res => {
-        this.weather = data;
-       });
-}
+
 
 restService.post("/audio", function(req, res) {
   var speech = "";
