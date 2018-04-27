65bae5305dd2ee44cdc5ecac90ef7ada59b380ff
ResulticksInc <mobile.sdk@resulticks.com>
2018-04-27 12:01:12 +0530

    issues fixed

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-import { callbackify } from "util";
-
 "use strict";
 
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const restService = express();
-
 
 restService.use(
   bodyParser.urlencoded({
@@ -16,28 +13,19 @@ restService.use(
 
 restService.use(bodyParser.json());
 
-
 restService.post("/echo", function(req, res) {
-  var tempCurr = '63';
-  var tempMax = '65';
-
-  var factCategory = req.body.result &&
-  req.body.result.parameters &&
-  req.body.result.parameters.echoText
-    ? req.body.result.parameters.echoText
-    : "Seems like some problem. Speak again.";
-
-  var speech =factCategory.toLowerCase();
-
-
-  
+  var speech =
+    req.body.result &&
+    req.body.result.parameters &&
+    req.body.result.parameters.echoText
+      ? req.body.result.parameters.echoText
+      : "Seems like some problem. Speak again.";
   return res.json({
     speech: speech,
     displayText: speech,
     source: "webhook-echo-sample"
   });
 });
-
 
 restService.post("/audio", function(req, res) {
   var speech = "";
@@ -210,4 +198,3 @@ restService.post("/slack-test", function(req, res) {
 restService.listen(process.env.PORT || 8000, function() {
   console.log("Server up and listening");
 });
-
