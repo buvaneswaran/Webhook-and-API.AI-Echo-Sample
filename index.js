e3a1016cbd9ff8471be7134ec43bd8f9e3232f9f
ResulticksInc <mobile.sdk@resulticks.com>
2018-04-27 12:32:38 +0530

    issue fixed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const https = require('https');
-const request = require('request');
+
 const restService = express();
 
 
@@ -17,14 +17,25 @@ restService.use(bodyParser.json());
 
 restService.post("/echo", function(req, res) {
 
-
-
-
-request('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', { json: true }, (err, res, body) => {
-  if (err) { return console.log(err); }
-  console.log(body.url);
-  console.log(body.explanation);
+  https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) => {
+  let data = '';
+ 
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+ 
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    console.log(JSON.parse(data).explanation);
+  });
+ 
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
 });
+
+
+
 
 
   var speech =
