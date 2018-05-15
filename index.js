af60853eca28369db8de75772a2344d402a52f19
Buvaneswaran <int.pbuvaneswaran@gmail.com>
2018-05-15 16:53:05 +0530

    issues

--- index.js
+++ index.js
@@ -31,15 +31,18 @@ restService.post("/home", function (req, res) {
   var speech = guessNum.toLowerCase();
 
   if (guessNum == 'welcome')
-    speech = 'Yes indeed. Today’s temperature reached a record high of 92 degrees outdoors. The temperature in this room is 78 degrees. Would you like me to adjust the air conditioning?';
+    speech = 'Yes indeed. Todays temperature reached a record high of 72 degrees outdoors, The temperature in this room is 68 degrees. Would you like me to switch on the air conditioning?';
   else if (guessNum == 'switchon')
-    speech = 'I’ve set the temperature to 72 degree. Do you want to change that?';
+    speech = 'I have set the temperature to 65 degree. Do you want to change that?';
   else if (guessNum == 'leave') {
-    speech = 'It’s time to replace your air conditioner filter. Vision H.V.A.C. has a 20 percent discount on filters for the next two weeks. Would you like to place an order now?';
+    speech = 'Its time to replace your air conditioner filter. Vision H.V.A.C. has a 20 percent discount on filters for the next two weeks. Would you like to place an order now?';
   } else if (guessNum == 'nextmonth') {
-    speech = 'Yes, I checked. The heat wave will continue through next month, would you like to order the replacement filters?';
+    speech = 'Yes, I checked. The heat wave will continue through next month, would you like to order the replacement filter?';
   } else if (guessNum == 'placeorder') {
-    speech = 'Ok. I have placed the order. Please check for the details on your email to complete payment.Your order has been placed and a confirmation message has been sent to you via Email and SMS';
+    speech = 'Ok. I have initiated an order. Please complete the order on your app and then check for the details on your email to complete payment.';
+   
+  }else if (guessNum == 'price') {
+    speech = 'Full price for a filter is $85.00. With the 20% discount it costs $68.00 and you will save $17.00. Delivery is free and you should receive it within 5 working days. Would you like to order the replacement filter?';
    
   }
   else if (guessNum == 'thanks'){
@@ -70,7 +73,7 @@ restService.post("/schedule", function (req, res) {
   if (guessNum == 'welcome')
     speech = 'you have a free schedule this evening.';
   else if (guessNum == 'offer')
-    speech = 'OK. Your Infinia creditcard entitles you to a 1+1 offer at Vision Kitchen in Phoenix Mall. Shall I connect you to the vision restaurants to make a booking? ';
+    speech = 'OK. Your Infinia creditcard entitles you to a 1+1 offer at Vision Kitchen in Phoenix Mall. Shall I connect you to the restaurant to make a booking? ';
   else if (guessNum == 'connect') {
     speech = 'Ok, connecting now.';
     request('http://resulticks.biz:81/Home/RegisterBank?id=hare_ram_end', { json: true }, (err, res, body) => { });
@@ -102,7 +105,7 @@ restService.post("/gartner", function (req, res) {
   if (guessNum == 'welcome')
     speech = 'OK. Multichannel marketing is being covered by Michael Lehmann and Max Russell.';
   else if (guessNum == 'report')
-    speech = 'The most recent report is the MQ from April 2017. Would you like the report summary?';
+    speech = 'The most recent report is the MQ from April 2018. Would you like the report summary?';
   else if (guessNum == 'readreport')
     speech = "Ok, here it is. Marketing leaders will find a host of new vendors in this year''s Magic Quadrant for multichannel campaign management. Vendors are focused on integrating machine learning, personalization and ad tech capabilities into big data foundations for deeper customer engagement.";
   else if (guessNum == 'sendemail') {
@@ -110,7 +113,7 @@ restService.post("/gartner", function (req, res) {
     speech = 'Sure, report has been emailed.';
   }
   else if (guessNum == 'schedule')
-    speech = 'Ok, I have matched your schedules. Best time slots available are next Wednesday 10am or next Friday 4pm with Max Russell. Michael Lehman is not available week after next. Do you want to book an appointment?';
+    speech = 'Ok, I have matched your schedules. Best time slots available are next Wednesday 10am or next Friday 4pm with Max Russell. Michael Lehman is not available the week after next. Do you want to book an appointment?';
   else if (guessNum == 'meeting') {
     request('http://resulticks.biz:81/Home/SendInvite?id=gg', { json: true }, (err, res, body) => { });
     speech = 'Meeting request has been sent.';
@@ -153,18 +156,18 @@ restService.post("/echo", function (req, res) {
   if (guessNum == 'campaign1') {
     //can you check if this weekends thanksgiving campaign has been triggered on resulticks
     type = 0;
-    speech = 'Yes, campaign triggered at 12pm on Wednesday, It is open till 30th April 2018. The performance reportis available. Would you like to read it?';
+    speech = 'Yes, campaign triggered at 12pm on Thursday, It is open till 30th April 2018. The performance reportis available. Would you like to read it?';
   } else if (guessNum == 'campaign2') {
     type = 1;
     speech ='Yes, campaign ended on April 22nd at 5pm. The performance report has been updated and is available. Would you like to read it?';
   } else if (guessNum == 'read') {
     if (type == 0)
-    speech ='Current status is Warming Up. Primary target is 10% achieved. Average reach rate is 43%. Average engagement rate is 1.5%. Average conversion rate is 0.05%. Would you like to read the recommendations';
+    speech ='Current status is Warming Up. Primary target is 10% achieved. Average reach rate is 43%. Average engagement rate is 1.5%. Average conversion rate is 0.05%. Would you like to hear the recommendations';
     else
-    speech ='Status is Achieved. Primary target achieved is 120%. Average reach rate is 36%. Average engagement rate is 9%. Average conversion rate is 3%. Would you like to read the recommendations?';
+    speech ='Status is Achieved. Primary target achieved is 120%. Average reach rate is 36%. Average engagement rate is 9%. Average conversion rate is 3%. Would you like to hear the recommendations?';
   } else if (guessNum == 'recommentations') {
     if (type == 0)
-    speech = 'Ok, here it is. The engagement rate has been low in comparison to the goal set. The campaign was blasted at 12pm Wednesday. But best practices indicate Friday evening to be best time for this campaign type.';
+    speech = 'Ok, here it is. The engagement rate has been low in comparison to the goal set. The campaign was blasted at 12pm Thursday. But best practices indicate Friday evening to be best time for this campaign type.';
     else
     speech ='Ok, here it is. The engagement rate has been low in comparison to the goal set. The campaign was blasted at 12pm Friday. But best practices indicate Thursday evening to be best time for this campaign type. Since the engagement rate was not achieved but the performance is better than industry standards we recommend making indicated changes during next campaign for better performance.';
   } else if (guessNum == 'report') {
